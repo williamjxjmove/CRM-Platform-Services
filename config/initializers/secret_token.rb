bc517488ee1c179adef5cbd341b5bdd950109666
Mike Linde <mike.linde@move.com>
2013-02-08 15:33:13 -0800

    Deleted old files/folders

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'c54f03ca8fa1cb571610be65814846f0bc6ab4cf5ac39ea12a3100b8d2984fb2522101756b0d9b4cd26ebb4f05b12b3f06e6ecddb1fb605213814000d7dd1572'
--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name                        "Mike Linde"
-    email                       "mike.linde@move.com"
-    password                    "foobar"
-    password_confirmation       "foobar"
-  end
-end
